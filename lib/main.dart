ea069ffac6917ab1f0849381fb2b346fc3111208
abc <abc@example.com>
2021-08-14 04:28:34 +0200

    initial repo

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(QuraanSchool());
+}
+
+class QuraanSchool extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('hello'));
+  }
+}
